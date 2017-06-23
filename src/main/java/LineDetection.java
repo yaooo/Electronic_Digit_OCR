f211f2b2036fb1f1130b9c971d903e9040ae0977
yao <yaoshi616@gmail.com>
2017-06-23 14:14:38 -0400

    Finish image processing, working on the location detection.

--- src/main/java/LineDetection.java
+++ src/main/java/LineDetection.java
@@ -1,27 +1,16 @@
-import static org.bytedeco.javacpp.helper.opencv_core.RGB;
 import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_highgui.cvDestroyAllWindows;
+import static org.bytedeco.javacpp.opencv_highgui.imshow;
 import static org.bytedeco.javacpp.opencv_highgui.waitKey;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_core.MatVector;
-import org.bytedeco.javacpp.opencv_core.Point;
-import org.bytedeco.javacpp.opencv_core.Scalar;
-import org.bytedeco.javacpp.indexer.FloatIndexer;
-import org.bytedeco.javacpp.indexer.IntIndexer;
-import org.bytedeco.javacpp.indexer.UByteIndexer;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 
@@ -38,53 +27,49 @@ public class LineDetection {
 
     public static void main(String [] args) throws IOException{
         String Path = "C:\\Users\\Yao\\Desktop\\testing\\rightbp.jpg";
-        String tempPath = "C:\\Users\\Yao\\Desktop\\testing\\temp.jpg";
+        String tempPath = Path.substring(0,Path.lastIndexOf('.'))+"_temp.jpg"; //Decide if deleting this file later
+
+
         Picture p = new Picture(Path);
         Mat original = imread(Path);
         imshow("original",original);
 
-        Mat blurred = GauissianBlur(p);
+        //RGB to Gray
+        Mat gray = new Mat();
+        cvtColor(original,gray,6);
+        imshow("gray",gray);
+
+        //Gaussian Blur
+        Mat blurred = GaussianBlur(gray);
         imshow("Blurred", blurred);
 
+        //canny edge detect
+        Mat contours = new Mat();
+        Canny(blurred, contours, 20, 40, 3,true); //TODO: adjust the threshold for the upper and lower bounds
+        imshow("canny",contours);
 
-        /*Mat contours = new Mat();
-        int threshold1 = 125;
-        int threshold2 = 350;
-        int apertureSize = 3;
-        Canny(blurred, contours, threshold1, threshold2, apertureSize,true);
-        imshow("canny",contours);*/
+        // TO Binary image
+        Mat BW = new Mat();
+        threshold(blurred,BW,120,255,0);
+        imshow("Binary image",BW);
+        Mat element5 = getStructuringElement(MORPH_ELLIPSE, new Size(15,15));
+
+        //Morphological closing
+        Mat closed = new Mat();
+        morphologyEx(BW, closed, MORPH_CLOSE, element5);
+        imshow("Closed",closed);
 
 
-        BufferedImage edge = CannyEdgeDetect(blurred,tempPath);
-        displayImage(edge);
-
+        waitKey(0);
 
     }
 
 
-    private static Mat GauissianBlur(Picture p) throws IOException{
-        BufferedImage gray = p.toGray(p.getBufferedImage());
-        String path = p.getPath();
-        String newPath = path.substring(0,path.lastIndexOf('.'))+"_Gray_temp.jpg";
-
-        File temp = new File(newPath);
-        if(temp.exists()){
-            if(!temp.delete()) System.out.println("Deletion fails.");
-        }
-
-        ImageIO.write(gray,"JPG", temp);
+    private static Mat GaussianBlur(Mat src){
         Mat dest = new Mat();
         Size KernelSize = new Size(3,3);
-        Mat src = imread(newPath);
         blur(src,dest,KernelSize);
         return dest;
-    }
-
-    private static void imshow(String txt, Mat img) {
-        CanvasFrame canvasFrame = new CanvasFrame(txt);
-        canvasFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        canvasFrame.setCanvasSize(img.cols(), img.rows());
-        canvasFrame.showImage(new OpenCVFrameConverter.ToMat().convert(img));
     }
 
     private static void displayImage(BufferedImage img2) {
@@ -99,10 +84,9 @@ public class LineDetection {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
+    } // Unused, keep it here just in case
 
     private static BufferedImage CannyEdgeDetect(Mat mat, String path){
-
         BufferedImage frame = matToBufferedImage(mat, path);
         System.out.println("dimension" + frame.getWidth() +" "+frame.getHeight());
 
@@ -117,7 +101,8 @@ public class LineDetection {
         detector.setSourceImage(frame);
         detector.process();
         return detector.getEdgesImage();
-    }
+    } // Use JavaCV default instead, DO NOT USE THIS
+
 
     //TODO: Replace this method for a better one later
     private static BufferedImage matToBufferedImage(Mat matBGR, String path) {
@@ -130,7 +115,6 @@ public class LineDetection {
         if(!new File(path).delete()) System.out.println("matToBufferedImage deletion fails.");
         return  buff;
     }
-
 
 }
 
